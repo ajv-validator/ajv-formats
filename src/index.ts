7facf9fc65045fd5b85d3fe825e34cffeba7495d
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-11-29 12:59:52 +0000

    set ajv option code.formats to support standalone code generation

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
 import formatLimit from "./limit"
 import type Ajv from "ajv"
 import type {Plugin, Format} from "ajv"
+import {_, Name} from "ajv/dist/compile/codegen"
 
 export {FormatMode, FormatName} from "./formats"
 export interface FormatOptions {
@@ -23,17 +24,21 @@ export interface FormatsPlugin extends Plugin<FormatsPluginOptions> {
   get: (format: FormatName, mode?: FormatMode) => Format
 }
 
+const fullName = new Name("fullFormats")
+const fastName = new Name("fastFormats")
+
 const formatsPlugin: FormatsPlugin = (
   ajv: Ajv,
   opts: FormatsPluginOptions = {keywords: true}
 ): Ajv => {
   if (Array.isArray(opts)) {
-    addFormats(ajv, opts, fullFormats)
+    addFormats(ajv, opts, fullFormats, fullName)
     return ajv
   }
-  const formats = opts.mode === "fast" ? fastFormats : fullFormats
+  const [formats, exportName] =
+    opts.mode === "fast" ? [fastFormats, fastName] : [fullFormats, fullName]
   const list = opts.formats || formatNames
-  addFormats(ajv, list, formats)
+  addFormats(ajv, list, formats, exportName)
   if (opts.keywords) formatLimit(ajv)
   return ajv
 }
@@ -45,7 +50,8 @@ formatsPlugin.get = (name: FormatName, mode: FormatMode = "full"): Format => {
   return f
 }
 
-function addFormats(ajv: Ajv, list: FormatName[], fs: DefinedFormats): void {
+function addFormats(ajv: Ajv, list: FormatName[], fs: DefinedFormats, exportName: Name): void {
+  ajv.opts.code.formats ??= _`require("ajv-formats/dist/formats").${exportName}`
   for (const f of list) ajv.addFormat(f, fs[f])
 }
 
