86d71ebedb4e1310f24d08faab012c7632127054
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-11-07 16:26:53 +0000

    pass all JSON Schema Test Suite tests

--- tests/json-schema.spec.js
+++ tests/json-schema.spec.js
@@ -2,30 +2,47 @@ const jsonSchemaTest = require("json-schema-test")
 const Ajv = require("ajv").default
 const addFormats = require("../dist")
 
-const ajv = new Ajv({$data: true, strictTypes: false, formats: {allowedUnknown: true}})
-addFormats(ajv)
-
-jsonSchemaTest(ajv, {
-  description: `JSON-Schema Test Suite draft-07 formats + extras`,
+jsonSchemaTest(getAjv(true), {
+  description: `JSON-Schema Test Suite formats`,
   suites: {
     "draft-07 formats": "./JSON-Schema-Test-Suite/tests/draft7/optional/format/*.json",
     "draft-07 regex": "./JSON-Schema-Test-Suite/tests/draft7/optional/ecmascript-regex.json",
     "draft-2019-09 formats": "./JSON-Schema-Test-Suite/tests/draft2019-09/optional/format/*.json",
     "draft-2019-09 regex":
       "./JSON-Schema-Test-Suite/tests/draft2019-09/optional/ecmascript-regex.json",
-    extras: "./extras/{**/,}*.json",
+    "draft-2020-12 formats": "./JSON-Schema-Test-Suite/tests/draft2020-12/optional/format/*.json",
+    "draft-2020-12 regex":
+      "./JSON-Schema-Test-Suite/tests/draft2020-12/optional/ecmascript-regex.json",
   },
   only: [],
   skip: ["format/idn-email", "format/idn-hostname", "format/iri", "format/iri-reference"],
-  afterEach({valid, errors}) {
-    expect(typeof valid).toBe("boolean")
-    if (valid === true) {
-      expect(errors).toBe(null)
-    } else {
-      expect(Array.isArray(errors)).toBe(true)
-      errors.every((err) => expect(typeof err).toBe("object"))
-    }
-  },
+  afterEach,
   cwd: __dirname,
   hideFolder: "draft7/",
 })
+
+jsonSchemaTest(getAjv(), {
+  description: `Extra tests`,
+  suites: {
+    extras: "./extras/{**/,}*.json",
+  },
+  only: [],
+  afterEach,
+  cwd: __dirname,
+})
+
+function getAjv(strictTime) {
+  const ajv = new Ajv({$data: true, strictTypes: false, formats: {allowedUnknown: true}})
+  addFormats(ajv, {mode: "full", keywords: true, strictTime})
+  return ajv
+}
+
+function afterEach({valid, errors}) {
+  expect(typeof valid).toBe("boolean")
+  if (valid === true) {
+    expect(errors).toBe(null)
+  } else {
+    expect(Array.isArray(errors)).toBe(true)
+    errors.every((err) => expect(typeof err).toBe("object"))
+  }
+}
